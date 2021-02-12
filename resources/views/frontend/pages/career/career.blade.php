010d1675909c17eaef3327cfc432e3b3a31fce4f
Parthkumar Khunt <parthkhunt12@gmail.com>
2021-02-12 09:21:36 +0530

    change in dynamic form filed

--- resources/views/frontend/pages/career/career.blade.php
+++ resources/views/frontend/pages/career/career.blade.php
@@ -1,102 +1,99 @@
 @extends('frontend.layout.app')
 @section('section')
 
-  <!-- CONTENT START -->
-  <section>
-    <!-- PROJECT GRID START -->
-    <div class="container mt-5 mb-5">
-       <div class="filter-container">
-           <ul class="filter">
-               <li class="active" data-filter="*">All</li>
-                @foreach($menu as $key => $value)
-                    <li class="" data-filter="{{  ".".str_replace(' ', '-',$value['name'])  }}">  {{ $value->name }} </a></li>
-                @endforeach
-           </ul>
-       </div>
-
-       <div class="grid grid-two-col" id="kehl-grid" style="width: 100% !important">
-           <div class="grid-sizer"></div>
-            @foreach($carrer as $key => $value)
-                <div class="grid-box {{  str_replace(' ', '-',$value['dept_name'])  }}" style="width: 100% !important;text-align: left;margin-bottom: 20px">
-
-                   <h4 style="color: #01B3E4">{{ $value['headline'] }}</h4>
-                   <div class="text">{{ $value['details']  }}</div>
-                   <div class="text">Experience : {{ $value['experience'] }}</div>
-                   <div class="text"><strong><u>Skills</u></strong></div>
-                   @php
-                   $skillsArray = explode(",",$value['skills_details']);
-                   @endphp
-                   <div class="" style="padding-left:  25px ;">
-
-                        @foreach($skillsArray as $key_skill =>  $val_skill)
-                        <div class="text" style="padding: 5px"> <i class="fa fa-arrow-right" aria-hidden="true"></i> {{ $val_skill }}</div>
-                        @endforeach
+  
+    <div class="container">
+        <div class="career-list-banner">
+            <h6>We have a range of amazing opportunities available</h6>
+            <h2>Join Our Team</h2>
+            <div class="career-contact"> 
+                <a href="" class="email">demo@gmail.com</a> 
+                <a href="" class="phone">+91 123456789</a>
+            </div>
+        </div>
+    </div>
+    <section class="career-list">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                    <div class="career-box" style="background-image: url(https://www.yudiz.com/wp-content/uploads/2020/01/react-developer.svg);">
+                        <h6>React Native Developer</h6>
+                        <p>MIN 2 YEARS</p>
+                        <a href="#"></a>
                     </div>
                 </div>
-            @endforeach
-       </div>
-
-       <div class="container mt-5 mb-5">
-
-<div class="row">
-    <div class="col-md-8 mx-auto">
-        <div class="contact-title">
-            <h3>Ask Your Question</h3>
-          
-        </div>
-        <br/>
-        <div id="alertDiv">
-
-        </div>
-        <form id="career-form-add" method="post" >
-            @csrf
-            <div class="messages"></div>
-            <div class="controls">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div class="form-group">
-                        <lable>Your full Name *</lable>
-                            <input id="form_name" type="text" name="name" class="form-control custom-form" placeholder="Enter your full name" required="required" data-error="Enter your full name.">
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="form-group">
-                        <lable>Your Email *</lable>
-                            <input id="form_email" type="email" name="email" class="form-control custom-form" placeholder="Enter your email" required="required" data-error="Enter your email.">
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="form-group">
-                        <lable>Your mobile number</lable>
-                            <input id="form_phone" type="tel" name="phone" class="form-control custom-form onlyNumber" placeholder="Enter your moblie number">
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="form-group">
-                        <lable>Attach your resume (Only .pdf files)*</lable>
-                            <input id="form_file" type="file" name="file" accept="application/pdf, application/vnd.ms-excel" class="form-control custom-form">
-                        </div>
-                    </div>
-                    <div class="col-sm-12">
-                        <div class="form-group">
-                        <lable>Leave your message ( Maxlength 240 characters )</lable>
-                            <textarea id="form_message" name="message" class="form-control message-form custom-form" placeholder="*Your message" rows="6" required="required" data-error="Please,leave us a message."></textarea>
-                        </div>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                    <div class="career-box" style="background-image: url(https://www.yudiz.com/wp-content/uploads/2019/12/career-ic-BA.svg);">
+                        <h6>Business Analyst</h6>
+                        <p>3 YEARS</p>
+                        <a href="#"></a>
                     </div>
                 </div>
-                <br/>
-                <div class="row">
-                    <div class="col-md-12 btn-send">
-                        <p><input type="submit" class="btn btn-washla" value="Send message"></p>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                    <div class="career-box" style="background-image: url(https://www.yudiz.com/wp-content/uploads/2020/07/python-developer.svg);">
+                        <h6>Python Developer</h6>
+                        <p>MIN 2 YEARS</p>
+                        <a href="#"></a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                    <div class="career-box" style="background-image: url(https://www.yudiz.com/wp-content/uploads/2020/01/react-developer.svg);">
+                        <h6>React JS Developer</h6>
+                        <p>1 - 5 YEARS</p>
+                        <a href="#"></a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                    <div class="career-box" style="background-image: url(https://www.yudiz.com/wp-content/uploads/2019/09/src_wordpress-dev.svg);">
+                        <h6>Jr. WordPress Developer</h6>
+                        <p>1 - 2 YEARS</p>
+                        <a href="#"></a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                    <div class="career-box" style="background-image: url(https://www.yudiz.com/wp-content/uploads/2019/09/src_node-js.svg);">
+                        <h6>Node JS Developer</h6>
+                        <p>1 - 2 YEARS</p>
+                        <a href="#"></a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                    <div class="career-box" style="background-image: url(https://www.yudiz.com/wp-content/uploads/2019/12/career-ic-laravel.svg);">
+                        <h6>Laravel Developer</h6>
+                        <p>1 - 2 YEARS</p>
+                        <a href="#"></a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                    <div class="career-box" style="background-image: url(https://www.yudiz.com/wp-content/uploads/2019/09/src_native-ios.svg);">
+                        <h6>iOS Developer</h6>
+                        <p>1 - 2 YEARS</p>
+                        <a href="#"></a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                    <div class="career-box" style="background-image: url(https://www.yudiz.com/wp-content/uploads/2019/12/career-ic-android.svg);">
+                        <h6>Android Developer</h6>
+                        <p>1 - 2 YEARS</p>
+                        <a href="#"></a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                    <div class="career-box" style="background-image: url(https://www.yudiz.com/wp-content/uploads/2019/06/career-ic-mean-stack.svg);">
+                        <h6>Mean Stack Developer</h6>
+                        <p>1 - 2 YEARS</p>
+                        <a href="#"></a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                    <div class="career-box other-career-box">
+                        <small>Not Listed Here?</small>
+                        <h6>Apply For Other</h6>
+                        <a href="#"></a>
                     </div>
                 </div>
             </div>
-        </form>
-    </div>
-</div>
-</div>
-   </div>
-   <!-- PROJECT GRID END -->
-</section>
-<!-- CONTENT END -->
+        </div>
+    </section>
+
 @endsection
