fb3134b34656ef5b781ea40d4406160537b0b07b
ParthkumarKhunt <parthkhunt12@gmail.com>
2021-03-03 12:40:04 +0530

    setup

--- resources/views/frontend/pages/team/team.blade.php
+++ resources/views/frontend/pages/team/team.blade.php
@@ -10,7 +10,19 @@
             <div class="col-md-6 col-lg-4">
                 <div class="team-card">
                     <figure class="tc-portrait">
-                    <img src="{{ asset('public/upload/ourteam/'. $value->image)}}" alt="ourteam">
+                        @php
+                                if($value->image != '' || $value->image != null ){
+                                    if(file_exists( public_path().'/upload/ourteam/'.$value->image) ){
+                                        $image = url("public/upload/ourteam/" . $value->image);
+                                    }else{
+                                        $image = url("public/frontend/assets/images/commons/user.png");
+                                    }
+                                }else{
+                                    $image = url("public/frontend/assets/images/commons/user.png");
+                                }
+                            @endphp
+
+                    <img src="{{ $image  }}" alt="ourteam" style="height:330px">
                             <ul class="tc-social">
                             @if($value->facebook != NULL)
                                 <li><a href="{{ $value->facebook }}"><i class="fab fa-facebook-f"></i></a></li>
